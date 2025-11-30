e03f33def992b2c2474ff6b391be521daa100d92
Luyun1224 <lu801224@gmail.com>
2025-11-30 03:06:38 +0000

    更新

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/NewSP-Training/', // 替換為您的 GitHub repository 名稱
+  base: '/NewSP-Training/',
 })
